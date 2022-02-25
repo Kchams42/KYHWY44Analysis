f3e9becfe824229f53910a5dee4abffac360bf23
Kyle Hamsley <kchams42@gmail.com>
2022-02-24 23:02:44 -0500

    Added registration data and continued code work

--- HWY44Data.py
+++ HWY44Data.py
@@ -1,20 +1,38 @@
+from unittest import result
 import pandas as pd
 import numpy
 import matplotlib
 
 #importing data
 df=pd.read_csv('venv\Analysis\Incident.txt')
-
+df2=pd.read_csv('venv\Analysis\Registration data - Sheet1.csv')
+df['year'] =pd.DatetimeIndex(df['CollisionDate']).year
 #Looking at different parts of the data set.
     #print (df.head)
     #print (df.info())
     #print (df.describe)
     #print (df['AgencyName'].value_counts())
-useful_columns = [19,20,22,23,24,]
-new_df = df[df.columns[useful_columns]]
+
+useful_columns = [39,22,23,24,]
+df = df[df.columns[useful_columns]]
+#print (df)
 #print (new_df.head)
 #print (new_df.info())
 #print ( new_df.describe)
 #print (new_df.shape)
-print (new_df['NumberInjured'].value_counts())
-#import registration data
+#print (new_df['MotorVehiclesInvolved'].value_counts())
+#print (new_df.sum())    
+df = df.groupby(['year']).sum()
+df = df.T
+df2=df2.T
+print (df2)
+print (df)
+
+
+
+
+
+#frames= [df, df2]
+#result = pd.concat(frames)
+
+#print(result)
